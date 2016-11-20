aaa7b5e474a44970f54119c1fb8bf451a8d44045
Ian Wigley <ian.wigley2011@gmail.com>
2016-11-20 16:32:14 +0000

    AI Update
    Update to make the Bear & Bluething bounce off the walls..

--- Nodes/Nodes/BlueThingy.ts
+++ Nodes/Nodes/BlueThingy.ts
@@ -3,6 +3,8 @@ import Rectangle = require("Rectangle");
 
 class BlueThingy extends Enemy {
 
+    private m_blueThingRect: Rectangle;
+
     constructor(xpos: number, ypos: number, speedx: number, gamesprites: HTMLCanvasElement, wall: Array<Rectangle>) {
         super(xpos, ypos, speedx, gamesprites, wall);
         this.m_name = "BlueThingy";
@@ -13,6 +15,27 @@ class BlueThingy extends Enemy {
         if (this.m_animTimer > 0.4) {
             this.m_frameX = (this.m_frameX + 1) % 7;
             this.m_animTimer = 0;
+        }
+        this.m_x += this.m_speedX;
+        this.m_y += this.m_speedY;
+
+        if (this.m_y > 400 ||
+            this.m_y < 0) {
+            this.m_speedY *= -1;
+        }
+
+        this.m_blueThingRect = new Rectangle(this.m_x + 10, this.m_y, 64, 64);
+        for (var i = 0; i < this.m_walls.length; i++) {
+            if (this.m_blueThingRect.Intersects(this.m_walls[i])) {
+                if (this.m_speedX > 0) {
+                    this.m_x -= 5;
+                    this.m_speedX *= -1;
+                }
+                else {
+                    this.m_x += 5;
+                    this.m_speedX *= -1;
+                }
+            }
         }
     }
 
--- Nodes/Nodes/SpringBear.ts
+++ Nodes/Nodes/SpringBear.ts
@@ -2,6 +2,9 @@
 import Rectangle = require("Rectangle");
 
 class SpringBear extends Enemy {
+
+    private m_springBearRect: Rectangle;
+
     constructor(xpos: number, ypos: number, speedx: number, gamesprites: HTMLCanvasElement, wall: Array<Rectangle>) {
         super(xpos, ypos, speedx, gamesprites, wall);
     }
@@ -11,6 +14,26 @@ class SpringBear extends Enemy {
         if (this.m_animTimer > 0.4) {
             this.m_frameX = (this.m_frameX + 1) % 4;
             this.m_animTimer = 0;
+        }
+        this.m_x += this.m_speedX;
+        this.m_y += this.m_speedY;
+
+        if (this.m_y > 400 ||
+            this.m_y < 0) {
+            this.m_speedY *= -1;
+        }
+        this.m_springBearRect = new Rectangle(this.m_x + 10, this.m_y, 64, 64);
+        for (var i = 0; i < this.m_walls.length; i++) {
+            if (this.m_springBearRect.Intersects(this.m_walls[i])) {
+                if (this.m_speedX > 0) {
+                    this.m_x -= 5;
+                    this.m_speedX *= -1;
+                }
+                else {
+                    this.m_x += 5;
+                    this.m_speedX *= -1;
+                }
+            }
         }
     }
 
