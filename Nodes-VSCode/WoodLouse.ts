bf2c322aa60ad1fd4169f309adc1c4514ad2a0c7
Ian Wigley <ian.wigley@outlook.com>
2022-12-11 14:12:57 +0000

    Update WoodLouse.ts
    Update WoodLouse.ts.

--- Nodes-VSCode/WoodLouse.ts
+++ Nodes-VSCode/WoodLouse.ts
@@ -13,19 +13,34 @@ class WoodLouse extends Enemy {
     ) {
         super(x, y, speed, gameSprites, wall, screenInfo);
         this.m_name = "WoodLouse";
+        this.m_facingLeft = true;
     }
 
     public Update(): void {
         this.m_animTimer += 0.1;
-        this.m_x += this.m_speed;
+        this.m_x -= this.m_speed;
         if (this.m_animTimer > 0.4) {
             this.m_frame = (this.m_frame + 1) % 4;
             this.m_animTimer = 0;
         }
+
+        if (this.m_x >= 700) {
+            this.m_speed *= -1;
+            this.m_facingLeft = true;
+        }
+        else if (this.m_x < 0) {
+            this.m_speed *= -1;
+            this.m_facingLeft = false;
+        }
     }
 
     public Draw(ctx: CanvasRenderingContext2D): void {
-        ctx.drawImage(this.m_texture, (this.m_frame + 4)* 64, 7 * 69, 68, 68, this.m_x, this.m_y, 64, 64);
+        if (!this.m_facingLeft) {
+            ctx.drawImage(this.m_texture, this.m_frame * 64, 7 * 69, 68, 68, this.m_x, this.m_y, 64, 64);
+        }
+        else {
+            ctx.drawImage(this.m_texture, (this.m_frame + 4) * 64, 7 * 69, 68, 68, this.m_x, this.m_y, 64, 64);
+        }
     }
 }
 export = WoodLouse;
