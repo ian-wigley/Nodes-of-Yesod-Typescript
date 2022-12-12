865b4c2bdba47a6471df9d31441359ff1f34c7fe
Ian Wigley <ian.wigley@outlook.com>
2022-12-12 08:41:30 +0000

    Update GreenMeanie.ts
    Update the GreenMeanie.ts.

--- Nodes-VSCode/GreenMeanie.ts
+++ Nodes-VSCode/GreenMeanie.ts
@@ -19,16 +19,30 @@ class GreenMeanie extends Enemy {
 
     public Update(): void {
         this.m_animTimer += 0.1;
-        // this.m_x += this.m_speed;
+        this.m_x += this.m_speed;
         this.m_offsetX = (this.m_frame + 11) * 64;
         if (this.m_animTimer > 0.4) {
             this.m_frame = (this.m_frame + 1) % 4;
             this.m_animTimer = 0;
         }
+        if (this.m_x >= 700) {
+            this.m_speed *= -1;
+            this.m_facingLeft = true;
+        }
+        else if (this.m_x < 0) {
+            this.m_speed *= -1;
+            this.m_facingLeft = false;
+        }
     }
 
     public Draw(ctx: CanvasRenderingContext2D): void {
+        if (!this.m_facingLeft) {
         ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
+        }
+        else {
+            this.m_offsetX += 4 * 64;
+            ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
+        }
     }
 }
 export = GreenMeanie;
