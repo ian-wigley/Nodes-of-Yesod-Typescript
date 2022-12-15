9ec449142749df82237139e2da0f5f8226731cf8
Ian Wigley <ian.wigley@outlook.com>
2022-12-15 14:09:52 +0000

    Create TelePort.ts
    Create TelePort.ts.

---
+++ Nodes-VSCode/TelePort.ts
@@ -0,0 +1,38 @@
+﻿import Enemy = require("Enemy");
+import Rectangle = require("Rectangle");
+import ScreenInfo = require("ScreenInfo");
+
+class TelePort extends Enemy {
+    constructor(
+        x: number,
+        y: number,
+        speed: number,
+        gameSprites: HTMLCanvasElement,
+        wall: Array<Rectangle>,
+        screenInfo: ScreenInfo
+    ) {
+        super(x, y, speed, gameSprites, wall, screenInfo);
+        this.m_name = "TelePort";
+        this.m_x = 100;//x;
+        this.m_y = y;
+        this.m_width = 34;
+        this.m_height = 64;
+        this.m_speed = speed;
+        this.m_offsetX = 11 * 64;
+        this.m_offsetY = 7 * 69;
+    }
+
+    public Update(): void {
+        this.m_animTimer += 0.1;
+        this.m_offsetX = (this.m_frame + 8) * 64;
+        if (this.m_animTimer > 0.4) {
+            this.m_frame = (this.m_frame + 1) % 3;
+            this.m_animTimer = 0;
+        }
+    }
+
+    public Draw(ctx: CanvasRenderingContext2D): void {
+        ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
+    }
+}
+export = TelePort; 
