35fb39c550a646c49990b2e2995f9c0f45c7f320
Ian Wigley <ian.wigley@outlook.com>
2023-02-05 18:52:36 +0000

    Update Caterpillar.ts
    Update Caterpillar.ts.

--- Nodes-VSCode/Caterpillar.ts
+++ Nodes-VSCode/Caterpillar.ts
@@ -20,9 +20,18 @@ class Caterpillar extends Enemy {
 
     public Update(): void {
         this.m_animTimer += 0.1;
+        this.m_x += this.m_speed;
         if (this.m_animTimer > 0.4) {
             this.m_frame = (this.m_frame + 1) % 7;
             this.m_animTimer = 0;
+        }
+        if (this.m_x >= 700) {
+            this.m_speed *= -1;
+            this.m_facingLeft = true;
+        }
+        else if (this.m_x < 0) {
+            this.m_speed *= -1;
+            this.m_facingLeft = false;
         }
     }
 
