86d68e73f5c6284e933c2fe8c4eaf65e470b6023
Ian Wigley <ian.wigley@outlook.com>
2023-03-13 07:40:35 +0000

    Update TelePort.ts
    Add the debug rectangle drawing functionality.

--- Nodes-VSCode/TelePort.ts
+++ Nodes-VSCode/TelePort.ts
@@ -22,7 +22,6 @@ class TelePort extends Enemy {
         this.m_height = 64;
         this.m_speed = speed;
         this.m_warpTo = warpTo;
-
         this.m_offsetX = 11 * 64;
         this.m_offsetY = 7 * 69;
     }
@@ -38,6 +37,7 @@ class TelePort extends Enemy {
 
     public Draw(ctx: CanvasRenderingContext2D): void {
         ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
+        this.DrawDebugRectangle(ctx);
     }
 }
 export = TelePort;
