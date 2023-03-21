78c6d92c132fe07c30fe09f9ddaa68edf3debc07
Ian Wigley <ian.wigley@outlook.com>
2023-03-21 07:36:36 +0000

    Create Tile.ts
    Add class that maps against the JSON.

---
+++ Nodes-VSCode/Tile.ts
@@ -0,0 +1,38 @@
+import Enemy = require("./Enemy");
+import Rectangle = require("./Rectangle");
+
+/**
+ * Class is used to Map against the JSON.
+ */
+class Tile {
+    name: string;
+    sx: number;
+    sy: number;
+    sw: number;
+    sh: number;
+    dx: number;
+    dy: number;
+    dw: number;
+    dh: number;
+    tiles = [{ "sx": 0, "sy": 0, "sw": 0, "sh": 0, "dx": 0, "dy": 0, "dw": 0, "dh": 0 }];
+    rectangleList = [{ "left": 0, "top": 0, "right": 0, "bottom": 0 }];
+    enemies: Array<Enemy>;
+    edibleWall: Array<Rectangle>;
+
+    constructor() {
+        this.name = "";
+        this.sx = 0;
+        this.sy = 0;
+        this.sw = 0;
+        this.sh = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.dw = 0;
+        this.dh = 0;
+        this.tiles = [{ "sx": 0, "sy": 0, "sw": 0, "sh": 0, "dx": 0, "dy": 0, "dw": 0, "dh": 0 }];
+        this.rectangleList = [{ "left": 0, "top": 0, "right": 0, "bottom": 0 }];
+        this.enemies = [];
+        this.edibleWall = [];
+    }
+}
+export = Tile;
