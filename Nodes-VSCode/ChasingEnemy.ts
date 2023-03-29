0bccddbcd646944de733110386dcb925ffeb06ce
Ian Wigley <ian.wigley@outlook.com>
2023-03-29 17:26:11 +0100

    Update ChasingEnemy.ts
    Update Chasing Enemy to extend the Non Directional enemy class.

--- Nodes-VSCode/ChasingEnemy.ts
+++ Nodes-VSCode/ChasingEnemy.ts
@@ -1,8 +1,8 @@
-﻿import Enemy = require("Enemy");
+﻿import NonDirectionalEnemy = require("NonDirectionalEnemy");
 import Rectangle = require("Rectangle");
 import ScreenInfo = require("ScreenInfo");
 
-class ChasingEnemy extends Enemy {
+class ChasingEnemy extends NonDirectionalEnemy {
     constructor(
         x: number,
         y: number,
@@ -13,7 +13,7 @@ class ChasingEnemy extends Enemy {
     ) {
         super(x, y, speed, gameSprites, wall, screenInfo);
         this.m_name = "ChasingEnemy";
-        this.m_offsetX = 0 * 64;
+        this.m_offsetX = 0;
         this.m_offsetY = 11 * 69;
         this.m_width = 34;
         this.m_height = 64;
@@ -22,6 +22,7 @@ class ChasingEnemy extends Enemy {
 
     public Update(): void {
         this.m_animTimer += 0.1;
+        this.m_offsetX = this.m_frame * 64;
         if (this.m_animTimer > 0.4) {
             this.m_frame = (this.m_frame + 1) % 3;
             this.m_animTimer = 0;
@@ -39,11 +40,6 @@ class ChasingEnemy extends Enemy {
         else if (this.m_charlieY > this.m_y) {
             this.m_y += this.m_speedY;
         }
-    }
-
-    public Draw(ctx: CanvasRenderingContext2D): void {
-        ctx.drawImage(this.m_texture, this.m_frame * 64, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
-        this.DrawDebugRectangle(ctx);
     }
 }
 
