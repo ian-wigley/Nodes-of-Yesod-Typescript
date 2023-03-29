220b042841d8e61a11f651ae03151b9bc6a3c7d5
Ian Wigley <ian.wigley@outlook.com>
2023-03-29 17:25:54 +0100

    Update RedSpaceman.ts
    Update Red Spaceman to extend the Non Directional enemy class.

--- Nodes-VSCode/RedSpaceman.ts
+++ Nodes-VSCode/RedSpaceman.ts
@@ -1,8 +1,8 @@
-﻿import Enemy = require("Enemy");
+﻿import NonDirectionalEnemy = require("NonDirectionalEnemy");
 import Rectangle = require("Rectangle");
 import ScreenInfo = require("ScreenInfo");
 
-class RedSpaceman extends Enemy {
+class RedSpaceman extends NonDirectionalEnemy {
     constructor(
         x: number,
         y: number,
@@ -13,8 +13,9 @@ class RedSpaceman extends Enemy {
     ) {
         super(x, y, speed, gameSprites, wall, screenInfo);
         this.m_name = "RedSpaceman";
-        this.m_offsetX = 0 * 64;
-        this.m_offsetY = 0 * 69;
+        this.m_offsetX = 0;
+        this.m_offsetY = 22 * 69;
+        this.m_imageIndex = 2 * 64;
         this.m_width = 34;
         this.m_height = 64;
     }
@@ -22,15 +23,11 @@ class RedSpaceman extends Enemy {
     public Update(): void {
         this.m_animTimer += 0.1;
         this.m_x += this.m_speed;
+        this.m_offsetX = this.m_frame * 64;
         if (this.m_animTimer > 0.4) {
-            this.m_frame = (this.m_frame + 1) % 4;
+            this.m_frame = (this.m_frame + 1) % 2;
             this.m_animTimer = 0;
         }
-    }
-
-    public Draw(ctx: CanvasRenderingContext2D): void {
-        ctx.drawImage(this.m_texture, this.m_frame * 64, 8 * 69, 68, 68, this.m_x, this.m_y, 64, 64);
-        this.DrawDebugRectangle(ctx);
     }
 }
 export = RedSpaceman;
