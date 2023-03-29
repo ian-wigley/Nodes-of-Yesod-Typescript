01bf8b3e30e3a950fe581450e6cbdff72e80497e
Ian Wigley <ian.wigley@outlook.com>
2023-03-29 17:27:48 +0100

    Update Plant.ts
    Update Plant to extend the Non Directional enemy class.

--- Nodes-VSCode/Plant.ts
+++ Nodes-VSCode/Plant.ts
@@ -1,8 +1,8 @@
-﻿import Enemy = require("Enemy");
-import Rectangle = require("Rectangle");
+﻿import Rectangle = require("Rectangle");
 import ScreenInfo = require("ScreenInfo");
+import NonDirectionalEnemy = require("./NonDirectionalEnemy");
 
-class Plant extends Enemy {
+class Plant extends NonDirectionalEnemy {
 
     private m_spawnTimer: number = 0;
 
@@ -16,7 +16,7 @@ class Plant extends Enemy {
     ) {
         super(x, y, speed, gameSprites, wall, screenInfo);
         this.m_name = "Plant";
-        this.m_offsetX = 0 * 64;
+        this.m_offsetX = 0;
         this.m_offsetY = 14 * 69;
         this.m_width = 34;
         this.m_height = 64;
@@ -35,11 +35,6 @@ class Plant extends Enemy {
         else {
             this.m_spawnTimer -= 1;
         }
-    }
-
-    public Draw(ctx: CanvasRenderingContext2D): void {
-        ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
-        this.DrawDebugRectangle(ctx);
     }
 }
 export = Plant;
