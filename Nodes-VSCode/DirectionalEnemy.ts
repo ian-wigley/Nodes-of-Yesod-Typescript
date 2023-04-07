315f4f5733ad0e2c104055253a4e123eac04c0b8
Ian Wigley <ian.wigley@outlook.com>
2023-04-07 11:29:12 +0100

    Update the Directional Enemy to use the Direction Enumeration.
    Update the Directional Enemy to use the Direction Enumeration.

--- Nodes-VSCode/DirectionalEnemy.ts
+++ Nodes-VSCode/DirectionalEnemy.ts
@@ -1,9 +1,10 @@
 import Enemy = require("Enemy");
+import { direction } from "./Direction";
 
 class DirectionalEnemy extends Enemy {
 
     public Draw(ctx: CanvasRenderingContext2D): void {
-        if (!this.m_facingLeft) {
+        if (this.m_direction == direction.FACE_RIGHT) {
             ctx.drawImage(this.m_texture, this.m_offsetX, this.m_offsetY, 68, 68, this.m_x, this.m_y, 64, 64);
         }
         else {
